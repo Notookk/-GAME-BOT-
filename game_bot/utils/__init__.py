50e732ef5aa489cdde735241773a4b41cb3aaa61
‹ 𝚲 𝚼 𝐈 › <164884298+Notookk@users.noreply.github.com>
2024-10-19 19:20:38 +0530

    Update __init__.py

--- game_bot/utils/__init__.py
+++ game_bot/utils/__init__.py
@@ -1,14 +1,7 @@
 from .game_state import GameState
 from .vote_counter import VoteCounter
-from .word_generator import generate_word
+from .word_loader import WordLoader
 from .authorization import is_admin
-from .token import generate_token, verify_token
+from .token import TokenManager
 
-__all__ = [
-    "GameState",
-    "VoteCounter",
-    "generate_word",
-    "is_admin",
-    "generate_token",
-    "verify_token",
-]
+__all__ = ["GameState", "VoteCounter", "WordLoader", "is_admin", "TokenManager"]
